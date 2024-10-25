d744081eb429f8ae169ac24fad4323912083875a
Lui5guilherme <lui5guilhermegalvaodab@gmail.com>
2024-10-25 17:41:45 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,284 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Master IA</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/swiper-bundle.min.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+</head>
+
+<body>
+    <!-- inicio do cabeçalho da home -->
+    <header>
+        <div class="nav container">
+            <a href="index.php" class="logo" title="Mater IA">
+                Master <span> IA</span>
+            </a>
+            <div class="busca-site">
+                <input type="search" id="busca-input" placeholder="Buscar Aulas">
+                <i class='bx bx-search'></i>
+            </div>
+            <a href="" class="user"></a>
+            <img src="assets/img/p1.png" alt="Luis Guilherme" class="user-img">
+            <div class="navbar">
+                <a href="#home" class="nav-link">
+                    <i class='bx bx-home'></i>
+                    <span class="nav-link-title">Home</span>
+                </a>
+
+                <a href="#popular" class="nav-link">
+                    <i class='bx bxs-hot'></i>
+                    <span class="nav-link-title">Em alta</span>
+                </a>
+
+                <a href="#aulas" class="nav-link">
+                    <i class='bx bx-compass'></i>
+                    <span class="nav-link-title">Explore</span>
+                </a>
+
+                <a href="#aulas" class="nav-link">
+                    <i class='bx bx-book-bookmark'></i>
+                    <span class="nav-link-title">Aulas</span>
+                </a>
+
+                <a href="#home" class="nav-link">
+                    <i class='bx bx-heart'></i>
+                    <span class="nav-link-title">Favoritos</span>
+                </a>
+            </div>
+        </div>
+    </header>
+    <!-- fim do cabeçalho -->
+
+
+    <section class="banner container">
+        <img src="assets/img/banner.png" alt="" class="banner-img">
+        <div class="banner-texto">
+            <h1 class="banner-titulo">Assista aos melhores conteudos sobre ia</h1>
+            <p>Especialistas da área conversan sobre o futuro das inteligências artificiais</p>
+            <a href="#" class="ver-btn">
+                <i class='bx bx-right-arrow'></i>
+                <span>Assista Agora</span>
+            </a>
+
+        </div>
+        </div>
+    </section>
+    <section class="popular container" id="popular">
+        <div class="topoaulas">
+            <h2 class="topotitulo">Em Alta</h2>
+            <div class="swiper-btn">
+                <div class="swiper-button-prev"></div>
+                <div class="swiper-button-next"></div>
+            </div>
+        </div>
+        <div class="popular-content swiper">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <div class="aula-box">
+                        <img src="assets/img/chatgpt-logo.jpg" alt="" class="aula-box-img">
+                        <div class="box-titulo">
+                            <h2 class="titulo-aula">ChatGPT</h2>
+                            <span class="categoria-aula">Aulas</span>
+                            <a href="ver-filme.php" class="ver-btn play-btn">
+                                <i class='bx bx-right-arrow'></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="aula-box">
+                        <img src="assets/img/fabio-akita.png" alt="" class="aula-box-img">
+                        <div class="box-titulo">
+                            <h2 class="titulo-aula">Fabio Akita</h2>
+                            <span class="categoria-aula">Podcast</span>
+                            <a href="ver-filme.php" class="ver-btn play-btn">
+                                <i class='bx bx-right-arrow'></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="aula-box">
+                        <img src="assets/img/Gemini.png" alt="" class="aula-box-img">
+                        <div class="box-titulo">
+                            <h2 class="titulo-aula">Gemini</h2>
+                            <span class="categoria-aula">Aulas</span>
+                            <a href="ver-filme.php" class="ver-btn play-btn">
+                                <i class='bx bx-right-arrow'></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="aula-box">
+                        <img src="assets/img/nvidia.jpg" alt="" class="aula-box-img">
+                        <div class="box-titulo">
+                            <h2 class="titulo-aula">NVIDIA Deep Learning AI</h2>
+                            <span class="categoria-aula">Aulas</span>
+                            <a href="ver-filme.php" class="ver-btn play-btn">
+                                <i class='bx bx-right-arrow'></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="aula-box">
+                        <img src="assets/img/alexa.png" alt="" class="aula-box-img">
+                        <div class="box-titulo">
+                            <h2 class="titulo-aula">Alexa</h2>
+                            <span class="categoria-aula">Aulas</span>
+                            <a href="ver-filme.php" class="ver-btn play-btn">
+                                <i class='bx bx-right-arrow'></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="aula-box">
+                        <img src="assets/img/daniellopez.jpg" alt="" class="aula-box-img">
+                        <div class="box-titulo">
+                            <h2 class="titulo-aula">daniel lopez</h2>
+                            <span class="categoria-aula">Podcast</span>
+                            <a href="ver-filme.php" class="ver-btn play-btn">
+                                <i class='bx bx-right-arrow'></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="aula-box">
+                        <img src="assets/img/Sérgio_Sacani.jpg" alt="" class="aula-box-img">
+                        <div class="box-titulo">
+                            <h2 class="titulo-aula">Sergio Sacani</h2>
+                            <span class="categoria-aula">Podcast</span>
+                            <a href="ver-filme.php" class="ver-btn play-btn">
+                                <i class='bx bx-right-arrow'></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="aulas container" id="lesson">
+        <div class="topoaulas">
+            <h2 class="topotitulo">Aulas e Podcasts</h2>
+        </div>
+        <div class="aulas-content">
+        <div class="swiper-slide">
+        <div class="aula-box">
+        <img src="assets/img/nvidia.jpg" alt="" class="aula-box-img">
+                        <div class="box-titulo">
+                            <h2 class="titulo-aula">NVIDIA Deep Learning AI</h2>
+                            <span class="categoria-aula">Aulas</span>
+                            <a href="ver-filme.php" class="ver-btn play-btn">
+                                <i class='bx bx-right-arrow'></i>
+                            </a>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="swiper-slide">
+                    <div class="aula-box">
+                    <img src="assets/img/chatgpt-logo.jpg" alt="" class="aula-box-img">
+                        <div class="box-titulo">
+                            <h2 class="titulo-aula">ChatGPT</h2>
+                            <span class="categoria-aula">Aulas</span>
+                            <a href="ver-filme.php" class="ver-btn play-btn">
+                                <i class='bx bx-right-arrow'></i>
+                            </a>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="swiper-slide">
+                    <div class="aula-box">
+                    <img src="assets/img/fabio-akita.png" alt="" class="aula-box-img">
+                        <div class="box-titulo">
+                            <h2 class="titulo-aula">Fabio Akita</h2>
+                            <span class="categoria-aula">Podcast</span>
+                            <a href="ver-filme.php" class="ver-btn play-btn">
+                                <i class='bx bx-right-arrow'></i>
+                            </a>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="swiper-slide">
+                    <div class="aula-box">
+                    <img src="assets/img/Sérgio_Sacani.jpg" alt="" class="aula-box-img">
+                        <div class="box-titulo">
+                            <h2 class="titulo-aula">Sergio Sacani</h2>
+                            <span class="categoria-aula">Podcast</span>
+                            <a href="ver-filme.php" class="ver-btn play-btn">
+                                <i class='bx bx-right-arrow'></i>
+                            </a>
+                        </div>
+                        </div>
+                        </div>
+                        <div class="swiper-slide">
+                    <div class="aula-box">
+                    <img src="assets/img/daniellopez.jpg" alt="" class="aula-box-img">
+                        <div class="box-titulo">
+                            <h2 class="titulo-aula">daniel lopez</h2>
+                            <span class="categoria-aula">Podcast</span>
+                            <a href="ver-filme.php" class="ver-btn play-btn">
+                                <i class='bx bx-right-arrow'></i>
+                            </a>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="swiper-slide">
+                    <div class="aula-box">
+                        <img src="assets/img/Midjourney_Emblem.png" alt="" class="aula-box-img">
+                        <div class="box-titulo">
+                            <h2 class="titulo-aula">Midjourney</h2>
+                            <span class="categoria-aula">Aulas</span>
+                            <a href="ver-filme.php" class="ver-btn play-btn">
+                                <i class='bx bx-right-arrow'></i>
+                            </a>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="swiper-slide">
+                    <div class="aula-box">
+                        <img src="assets/img/face.png" alt="" class="aula-box-img">
+                        <div class="box-titulo">
+                            <h2 class="titulo-aula">Reinforcement Learning by Facebook A</h2>
+                            <span class="categoria-aula">Aulas</span>
+                            <a href="ver-filme.php" class="ver-btn play-btn">
+                                <i class='bx bx-right-arrow'></i>
+                            </a>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="swiper-slide">
+                    <div class="aula-box">
+                        <img src="assets/img/cortana.png" alt="" class="aula-box-img">
+                        <div class="box-titulo">
+                            <h2 class="titulo-aula">Cortana</h2>
+                            <span class="categoria-aula">Aulas</span>
+                            <a href="ver-filme.php" class="ver-btn play-btn">
+                                <i class='bx bx-right-arrow'></i>
+                            </a>
+                        </div>
+                    </div>
+                    </div>
+        </div>
+    </section>
+<div class="proxima-pagina">
+    <a href="" class="proxima-btn">Proxima Pagina</a>
+</div>
+
+    <footer class="direitos">
+        <p>&#169; todos os direitos reservados ao grupo de tcc da etec de caieiras</p>
+    </footer>
+
+
+    <script src="assets/js/swiper-bundle.min.js"></script>
+    <script src="assets/js/scripts.js"></script>
+</body>
+
+</html>
